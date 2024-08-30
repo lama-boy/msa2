886dd4dfb6b9d4b091e8c81994abae7d2aeb455c
ysl2884 <ysl2884@gmail.com>
2024-08-30 23:38:24 +0900

    preparing api gateway

--- src/main/java/msa/prj/msa2/controller/MainController.java
+++ src/main/java/msa/prj/msa2/controller/MainController.java
@@ -2,15 +2,20 @@ package msa.prj.msa2.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class MainController {
     
     @GetMapping("/")
-    public String getMethodName() {
+    public String index() {
         return "index";
     }
-    
 
+    @GetMapping("/home")
+    @ResponseBody
+    public String home() {
+        return "home";
+    }
 }
